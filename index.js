7b4fcafde37fff72c99e238926b36107e06f8c0a
vSxF7 <victorrxavierr7@gmail.com>
2024-09-11 08:46:15 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const start = () => {
+    while(true){
+        let opcao = "sair"
+        switch(opcao){
+            case "cadastrar":
+                console.log("Vamos cadastrar")
+                break
+            case "listar":
+                console.log("Vamos listar")
+                break
+            case "sair":
+                return
+        }
+    }dasdas
+}
+
+start()
